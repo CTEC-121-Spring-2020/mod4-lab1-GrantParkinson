113702a7b3fa0407fcd1bd88f6f21b7203214766
Grant Parkinson <grant.c.parkinson@gmail.com>
2020-05-11 12:27:54 -0700

    finished mod lab

--- Template.py
+++ Template.py
@@ -1,7 +1,7 @@
 """
 CTEC 121
-<your name>
-<assignment/lab name>
+<Grant Parkinson>
+<Mod4 Lab 1>
 <assignment/lab description
 """
 
@@ -11,7 +11,33 @@ Process: description of what function does
 Output: return value and description
 """
 
-def main():
-    # code here
 
-main()    
+
+
+from graphics import *
+def main():
+    '''
+    win = GraphWin("Title", 600, 300)
+
+    p = Point(100, 200)
+    x = p.getX()
+    y = p.getY()
+    print("x and y :", x, y)
+
+    input()
+    '''
+
+    win = GraphWin("Grant's Window", 600, 600)
+
+    p1 = Point(50, 50)
+    p2 = Point(100, 100)
+    r1 = Rectangle(p1, p2)
+    r1.draw(win)
+
+    r2 = Rectangle(Point(200, 200), Point(250, 250))
+    r2.draw(win)
+
+    input()
+
+
+main()
