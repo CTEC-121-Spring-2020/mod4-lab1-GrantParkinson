4c7fdbe7dc086a7da97ba87317ae89212d72627e
Grant Parkinson <grant.c.parkinson@gmail.com>
2020-05-11 13:49:41 -0700

    fixed parentheses

--- converter.py
+++ converter.py
@@ -14,26 +14,26 @@ def main():
 
     # create text boxes
     Text(Point(150, 50), celsiusTempString).draw(win)
-    Text(Point(150, 250, fahrenheitTempString).draw(win)
+    Text(Point(150, 250), fahrenheitTempString).draw(win)
 
     # create center box
     Rectangle(Point(125, 100), Point(275, 200)).draw(win)
 
     # create button text
-    buttonText=Text(Point(200, 150), "Convert It")
+    buttonText = Text(Point(200, 150), "Convert It")
     buttonText.draw(win)
 
     # create input and output fields
-    inputCelsiusField=Entry(Point(300, 50), 5)
+    inputCelsiusField = Entry(Point(300, 50), 5)
     inputCelsiusField.setText("0.0")
     inputCelsiusField.draw(win)
 
-    outputFahrenheitField=Text(Point(300, 250), "")
+    outputFahrenheitField = Text(Point(300, 250), "")
     outputFahrenheitField.draw(win)
     win.getMouse()
 
-    celsiusTemperature=float(inputCelsiusField.getText())
-    fahrenheit=9.0/5.0 * celsiusTemperature + 32
+    celsiusTemperature = float(inputCelsiusField.getText())
+    fahrenheit = 9.0/5.0 * celsiusTemperature + 32
 
     # display results
     outputFahrenheitField.setText(round(fahrenheit, 2))
@@ -43,5 +43,6 @@ def main():
 
     # wait for final click to end
     win.getMouse()
+
 
 main()
